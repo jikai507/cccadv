0c109a076c9a08f4936fcd84556666ced6a13bab
jikai507 <jikai507@126.com>
2020-10-17 21:36:37 +0800

    now editor's tab can be worked

--- panel/editor.js
+++ panel/editor.js
@@ -1,3 +1,5 @@
+const advEditorContext = {};
+
 function updateContentPanelSize() {
     const w = document.body.offsetWidth;
     const h = document.body.offsetHeight;
@@ -9,3 +11,27 @@ function updateContentPanelSize() {
 window.onresize = function() {
     updateContentPanelSize();
 }
+
+function onSwitchDataCategory(category) {
+    if (advEditorContext["cur-data-category"] === category) {
+        return;
+    }
+    const tabs = ["plot", "character", "background", "audio", "item", "property"];
+    for (let i = 0; i < tabs.length; i++) {
+        const tabCategory = tabs[i];
+        const tabID = `${tabCategory}-tab`;
+        const a = document.getElementById(tabID);
+        if (tabCategory === category) {
+            a.className = "item active";
+        } else {
+            a.className = "item";
+        }
+    }
+    advEditorContext["cur-data-category"] = category
+}
+
+function main() {
+    $('.ui.dropdown').dropdown();
+}
+
+main();
